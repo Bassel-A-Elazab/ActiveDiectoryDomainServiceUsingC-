cc3ccd937887c49fb91c75af7a9089cbfc6b1a5b
Bassel Elazab <bassel.alazab@gmail.com>
2021-04-16 23:22:44 +0200

    the main class to run the all operations

---
+++ Assignmennt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+
+
+namespace Assignmennt
+{
+    public static class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var oldUser = new List<string>();
+            var updateUser = new List<string>();
+
+            oldUser.Add("Tamer");
+            oldUser.Add("Ali");
+            oldUser.Add("tamer.ali");
+            oldUser.Add("Ashraf#96");
+            User.addNewUser(oldUser);
+
+            updateUser.Add("TamerSelim");
+            updateUser.Add("Ali");
+            updateUser.Add("tamer.ali");
+            updateUser.Add("Ashraf#96");
+            User.upateNewUser(oldUser, updateUser);
+
+            User.searchForUser("tamer");   // display and seach in once.
+
+
+        }
+    }
+}
